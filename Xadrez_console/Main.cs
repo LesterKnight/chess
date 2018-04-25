d27f6120a18b94dcf5d207337ab115ec49f62dd8
Cristiano Miranda <cmoliveira2@latam.stefanini.com>
2018-04-25 15:59:28 -0300

    excecao personalizada

--- Xadrez_console/Main.cs
+++ Xadrez_console/Main.cs
@@ -10,6 +10,7 @@ namespace Xadrez_console
 
             tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(0, 0));
             tab.colocarPeca(new Cavalo(tab,Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(1, 4));
 
             Tela.imprimirTabuleiro(tab);
 
